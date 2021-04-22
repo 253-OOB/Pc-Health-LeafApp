4fb68d4a01c912125a15d5d2ade63604f7dc8b04
luaygharzeddine <luaygharzeddine@gmail.com>
2021-04-22 22:23:46 +0300

    Bug fixes, tested code

--- scripts/notifications/leaf_notification.py
+++ scripts/notifications/leaf_notification.py
@@ -1,3 +1,4 @@
+import json
 from time import time
 
 
@@ -35,44 +36,47 @@ class Notification(object):
         """
 
         if "processors" in reading:
-            self.__processors(reading)
+            return self.__processors(reading)
         elif "logical_disks" in reading:
-            self.__logical_disk(reading)
+            return self.__logical_disk(reading)
         elif "memory" in reading:
-            self.__memory(reading)
+            return self.__memory(reading)
 
     def __processors(self, reading):
         notifs = []
-        cores = reading["data"][0]["Cores"]
+        data = json.loads(reading["processors"]["data"])
+        cores = data[0]["Cores"]
         for core in cores:
             if core["Name"] == "_Total":
                 total_perc_proc_time = core["PercentProcessorTime"]
                 break
         operator = self.conf["notifications"]["PercentProcessorTime"]["Comparison"]["operator"]
         comparison_value = self.conf["notifications"]["PercentProcessorTime"]["Comparison"]["value"]
-        if should_trigger(operator, total_perc_proc_time, comparison_value):
-            notif = generate_notif(reading, "PercentProcessorTime")
+        if self.should_trigger(operator, total_perc_proc_time, comparison_value):
+            notif = self.generate_notif(reading, "PercentProcessorTime")
             notifs.append(notif)
         return notifs
 
     def __logical_disk(self, reading):
         notifs = []
-        for disk in reading["data"]:
+        data = json.loads(reading["logical_disks"]["data"])
+        for disk in data:
             free_space = disk["PercentFreeSpace"]
             operator = self.conf["notifications"]["LogicalDisk"]["Comparison"]["operator"]
             comparison_value = self.conf["notifications"]["LogicalDisk"]["Comparison"]["value"]
-            if should_trigger(operator, free_space, comparison_value):
-                notif = generate_notif(reading, "LogicalDisk")
+            if self.should_trigger(operator, free_space, comparison_value):
+                notif = self.generate_notif(reading, "LogicalDisk")
                 notifs.append(notif)
         return notifs
 
     def __memory(self, reading):
         notifs = []
-        free_mem = reading["data"][0]["AvailableMBytes"]
+        data = json.loads(reading["memory"]["data"])
+        free_mem = data[0]["AvailableMBytes"]
         operator = self.conf["notifications"]["Memory"]["Comparison"]["operator"]
         comparison_value = self.conf["notifications"]["Memory"]["Comparison"]["value"]
-        if should_trigger(operator, free_mem, comparison_value):
-            notif = generate_notif(reading, "Memory")
+        if self.should_trigger(operator, free_mem, comparison_value):
+            notif = self.generate_notif(reading, "Memory")
             notifs.append(notif)
         return notifs
 
@@ -98,3 +102,13 @@ class Notification(object):
         notif["Title"] = self.conf["notifications"][notif_type]["Title"]
         notif["Content"] = self.conf["notifications"][notif_type]["Content"]
         return notif
+
+
+with open("get_logical_disk_reading.json") as f:
+    cpu_data = json.load(f)
+with open("leaf_sample.json") as f:
+    config = json.load(f)
+
+
+notif = Notification(config["configuration"]["rules"])
+print(notif.parse(cpu_data))
